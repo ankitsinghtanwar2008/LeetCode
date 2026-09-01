da5862aa94e6c43f88243d9f5b765e4a8ffec055
Ankit Singh <ankitsinghtanwar2008@gmail.com>
2026-09-01 18:22:38 +0530

    Time: 1 ms (78.16%), Space: 42.5 MB (34.51%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int sumOfSquares(int n) {
+        int sum = 0;
+        while (n > 0) {
+            int digit = n % 10;
+            sum += digit * digit;
+            n /= 10;
+        }
+        return sum;
+    }
+    public boolean isHappy(int n) {
+        int slow = n;
+        int fast = n;
+        while (true) {
+            slow = sumOfSquares(slow);
+            fast = sumOfSquares(sumOfSquares(fast));
+            if (fast == 1) {
+                return true;
+            }
+            if (slow == fast) {
+                return false;
+            }
+        }
+    }
+}
