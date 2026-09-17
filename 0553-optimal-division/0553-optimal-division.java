cf009c4cca5b8f405fa0f7da6715f1ac7f9057c4
Ankit Singh <ankitsinghtanwar2008@gmail.com>
2026-09-17 16:00:07 +0530

    Time: 4 ms (40.41%), Space: 43 MB (53.42%) - LeetHub

---
+++ 0553-optimal-division/0553-optimal-division.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String optimalDivision(int[] nums) {
+        int n = nums.length;
+
+        if (n == 1) {
+            return String.valueOf(nums[0]);
+        }
+
+        if (n == 2) {
+            return nums[0] + "/" + nums[1];
+        }
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(nums[0]).append("/(");
+
+        for (int i = 1; i < n; i++) {
+            if (i > 1) {
+                sb.append("/");
+            }
+            sb.append(nums[i]);
+        }
+
+        sb.append(")");
+
+        return sb.toString();
+    }
+}
